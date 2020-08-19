052be37f8a462dc12c56566e84aeb2136d513a74
Habibajmal <60894911+Habibajmal@users.noreply.github.com>
2020-08-19 00:16:15 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Test from "./components/Test";
+import { Grid } from "@material-ui/core";
+function App() {
+  return (
+    <Grid container spacing={5}>
+      <Grid item xs={2}></Grid>
+      <Grid item xs={8}>
+        <div style={{ textAlign: "center" }}>
+          <h1>Assignment 2</h1>
+
+        </div>
+      </Grid>
+      <Grid item xs={2}></Grid>
+
+      <Grid item xs={1}></Grid>
+      <Grid item xs={10}>
+        <Test></Test>
+      </Grid>
+      <Grid item xs={1}></Grid>
+    </Grid>
+  );
+}
+
+export default App;
